10beb587da626220ba2fc162023dcb5f1d4cf9af
a.a.pligina <87080361+AnastasiaPlg@users.noreply.github.com>
2022-05-11 13:56:48 +0300

    Update TaskFromGit4.java

--- Task_fromGit4/src/TaskFromGit4.java
+++ Task_fromGit4/src/TaskFromGit4.java
@@ -2,7 +2,7 @@ public class TaskFromGit4 {
     public static void main(String[] args) {
         int n = 0;
         String number;
-        for (int i = 1; i <= 100000; i++) {
+        for (int i = 1; i < 100000; i++) {
             number = i + "";
             if (number.indexOf("13") >=0 || number.indexOf("4") >=0) {
                 n++;
